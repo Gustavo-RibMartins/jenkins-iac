70c68fce71c876bef52148e89a0ce32a4cd61fa6
Gustavo-RibMartins <guhrm96@gmail.com>
2023-03-11 12:35:45 -0300

    Fix: backend.tf

--- jenkins.tf
+++ jenkins.tf
@@ -3,7 +3,7 @@ module "jenkins_sg" {
 
   name        = "jenkins-sg"
   description = "Security group para o servidor do Jenkins Server"
-  vpc_id      = "vpc-085b125ed7a73368f"
+  vpc_id      = "vpc-0a72402f453b7ed9d"
 
   ingress_cidr_blocks = ["0.0.0.0/0"]
   ingress_rules       = ["http-80-tcp", "ssh-tcp"]
@@ -21,7 +21,7 @@ module "jenkins_ec2_instance" {
   key_name               = "vockey"
   monitoring             = true
   vpc_security_group_ids = [module.jenkins_sg.security_group_id]
-  subnet_id              = "subnet-016c2b7adad4a6a7f"
+  subnet_id              = "subnet-01a8845d467f43ca9"
   user_data              = file("./dependencias.sh")
   iam_instance_profile   = "LabInstanceProfile"
 
